3691cee65889f970ca0151d22c2e6a215227c481
Theo Golgevit <theo.golgevit@epita.fr>
2018-10-12 12:00:38 +0200

    typedef

---
+++ network.h
@@ -0,0 +1,9 @@
+# ifndef NETWORK_H
+# define NETWORK_H
+
+typedef struct Network Network;
+struct Network {
+
+};
+
+#endif
---
+++ nr.h
@@ -0,0 +1,11 @@
+#ifndef NR_H
+#define NR_H
+
+typedef struct malo malo
+struct malo {
+
+    float *bias;
+    float *weight;
+}
+
+#endif
