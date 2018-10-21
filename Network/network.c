95959f3eff96e3ff546b46b3d098aff1583bc583
Theo Golgevit <theo.golgevit@epita.fr>
2018-10-21 11:51:01 +0200

    help and backprop

--- Network/network.c
+++ Network/network.c
@@ -17,6 +17,7 @@ int main(){
     
     int trainingSet[] = {0, 0, 0, 1, 1, 0, 1, 1};
     double expectedResults[] = {0, 1, 1, 0};
+    double trainingStep = 0.1;
 
     /********************************/
     /********* NETWORK INIT *********/
@@ -40,11 +41,11 @@ int main(){
 
     net.weights = weights;
 
-    double nr[] = {2, 2, 1};
-    net.count_nr = nr;
+    double count_nr[] = {2, 2, 1};
+    net.count_nr = count_nr;
 
-    double weight[] = {1, 2, 2};
-    net.count_weight = weight;
+    double count_weight[] = {1, 2, 2};
+    net.count_weight = count_weight;
 
     double bias[5] = {0, 0, 0, 0, 0};
     net.bias = bias;
@@ -71,15 +72,37 @@ int main(){
 
     }
 
+    size_t testIndex = 0;
+    size_t testMaxCount = sizeof(expectedResults) / sizeof(expecteResults[0]);
+    double newWeights[] = calloc(8, sizeof(double));
+    // Weights: [ in0.0,  in1.0, hid0.0, hid0.1, hid1.0, hid1.1, out0.0, out0.1]
+    //          [ neur0,  neur1,  neur2,  neur2,  neur3,  neur3,  neur4,  neur4]
+    //          [    w0,     w1,     w2,     w3,     w4,     w5,     w6,     w7]
+    //          [i -> 0, i -> 1, 0 -> 2, 0 -> 3, 1 -> 2, 1 -> 3, 2 -> 4, 3 -> 4]
+
+
     /********************************/
     /********* PROPAGATION **********/
     /********************************/
 
 
-     /********************************/
-     /******* BACKPROPAGATION ********/
-     /********************************/
- 
+    /********************************/
+    /******* BACKPROPAGATION ********/
+    /********************************/
+    
+    // 1 - Calcul de l'erreur sortie
+    
+    double outputError = (expectedResults[testIndex] - net.computed[4]) * net.computed[4] * (1 - net.computed[4]);
+    printf("output error = %f\n", outputError);
+
+    // 2 - Poids sortie
+    
+    newWeights[7]
+
+    // 3 - Calcul de l'erreur couche cachee
+    
+    // 4 - Poids caches
+    
 }
 
 
