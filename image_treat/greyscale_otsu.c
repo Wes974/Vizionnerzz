8e2ed73e6a723694228f9814a36b163b6a70eb50
Mathieu Bouchet <mathieu.bouchet@r03p10.vj306.sm.cri.epita.net>
2018-10-21 11:41:39 +0200

    Finished
    Signed-off-by: Mathieu Bouchet <mathieu.bouchet@r03p10.vj306.sm.cri.epita.net>

--- image_treat/greyscale_otsu.c
+++ image_treat/greyscale_otsu.c
@@ -80,7 +80,7 @@ unsigned char otsu_treshold(Uint8 array[], size_t rows, size_t col)
 void otsu(Uint8 image[], Uint8 b_image[], size_t rows, size_t col)
 {
     Uint8 treshold = otsu_treshold(image, rows, col);
-    printf("Treshold: %d\n", treshold);
+    //printf("Treshold: %d\n", treshold);
     
     for(size_t i = 0; i < rows; i++)
     {
@@ -94,26 +94,6 @@ void otsu(Uint8 image[], Uint8 b_image[], size_t rows, size_t col)
     }
 }
 
-/*SDL_Surface* grayscale2(SDL_Surface *surf)
-{
-    SDL_Surface* gray;
-    gray = SDL_CreateRGBSurface(0, surf->w, surf->h, 32, 0, 0, 0, 0);
 
-    for(int i = 0; i < surf->h; i++)
-    {
-        for(int j = 0; j < surf->w; j++)
-        {
-            Uint32 pixel = get_pixel(surf, j, i);
-            Uint8 r, g, b;
-            SDL_GetRGB(pixel, surf->format, &r, &g, &b);
-            //printf("r: %d g: %d b: %d\n", r, g, b);
-            Uint8 color = 0.3 * r + 0.59 * g + 0.11 * b;
-            Uint32 newPixel = SDL_MapRGB(surf->format, color, color, color);
-            put_pixel(gray, j, i, newPixel);
-        }
-    }
-
-    return gray;
-}*/
 
 
--- image_treat/tests.c
+++ image_treat/tests.c
@@ -1,90 +1,40 @@
 #include <stdlib.h>
+#include <err.h>
 #include "Basics.h"
 #include "for_show.h"
 #include "greyscale_otsu.h"
 
-int main()
+int main(int argc, char** argv)
 {
+    char* param = argv[1];
+
+    if(argc != 2)
+        errx(1, "Too many arguments");
+
     SDL_Surface* image;
-    image = load_image("image1.bmp");
+    image = load_image(param);
+
+    if(image == NULL)
+        printf("Load failed: %s\n", SDL_GetError());
     
     int i = image->h;
     int j = image->w;
     Uint8 *gray = calloc(i * j, sizeof(Uint8));
 
-    printf("Image Base:\n");
-
-    /*for(int k = 0; k < i; k++)
-    {
-        for(int l = 0; l < j; l++)
-        {
-            Uint32 pixel = get_pixel(image, k, l);
-            Uint8 r, g, b;
-            SDL_GetRGB(pixel, image->format, &r, &g, &b);
-            printf("%d ", r);
-        }
-        
-        printf("\n");
-    }
-    printf("taille image: %d x %d\n", i, j);*/
-
     grayscale(image, gray, i, j);
-    /*SDL_FreeSurface(image);
-    
-    printf("Tableau:\n");
-
-    Print_Array(gray, j, i);*/
-    /*for(int k = 0; k < i + j; k++)
-        printf("rgb value: %d\n", array[k]);*/
     SDL_Surface* b_image;
     b_image = Matrix_2_Surface(gray, i, j);
     
-    /*printf("Image Greyscaled:\n");
-
-    for(int k = 0; k < i; k++)
-    {
-        for(int l = 0; l < j; l++)
-        {
-            Uint32 pixel = get_pixel(b_image, k, l);
-            Uint8 r, g, b;
-            SDL_GetRGB(pixel, b_image->format, &r, &g, &b);
-            printf("%d ", r);
-        }
-        
-        printf("\n");
-    }
-    
-    for(int m = 0; m < i; m++)
-    {
-        for(int n = 0; n < j; n++)
-        {
-            Uint32 newPixel = SDL_MapRGB(b_image->format, 0, 0, 0);
-            put_pixel(b_image, m, n, newPixel);
-        }
-    }*/
-
-    Make_A_File(b_image, "grayscaled.bmp");
-    //SDL_FreeSurface(b_image);
+    Make_A_File(b_image, "results/grayscaled.bmp");
     
     Uint8 *otsu_array = calloc(i * j, sizeof(Uint8));
     otsu(gray, otsu_array, i, j);
-    //free(gray);
-    /*printf("Tableau:\n");
-    Print_Array(otsu_array, j, i);*/
+    free(gray);
 
     SDL_Surface* f_image;
     f_image = Matrix_2_Surface(otsu_array, i, j);
-    //free(otsu_array);
-    Make_A_File(f_image, "binarized.bmp");
-    //SDL_FreeSurface(f_image);
+    free(otsu_array);
+    Make_A_File(f_image, "results/binarized.bmp");
 
-
-
-
-    /*SDL_Surface* gray = NULL;
-    gray = grayscale2(image);
-    //SDL_FreeSurface(image);
-    Make_A_File(gray, "grayscaled.bmp");
-    //SDL_FreeSurface(gray);*/
-
+    return 0;
 }
