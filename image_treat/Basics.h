7287277695457ea971ed9e74ff3c953067ff1b97
Mathieu Bouchet <mathieu.bouchet@epita.fr>
2018-12-02 15:11:34 +0100

    Basic+
    Signed-off-by: Mathieu Bouchet <mathieu.bouchet@epita.fr>

---
+++ image_treat/Basics.h
@@ -0,0 +1,12 @@
+#ifndef BASICS_H_
+#define BASICS_H_
+
+#include <stdlib.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+
+SDL_Surface* load_image(char *path);
+Uint32 get_pixel(SDL_Surface *surface, unsigned x, unsigned y);
+void put_pixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
+
+#endif
